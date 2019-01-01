7efbfaba15727e572c1f85019028e10cfc08867c
Alaaddin Hiefield <alaaddinhiefield@Alaaddins-MacBook-Pro-2.local>
2018-12-31 18:21:20 -0800

    formatted JSON

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const keys = require('dotenv').config();  // Get environment variables (API Key)
 const api_key = process.env.API_KEY;      // store API key for google maps
 
-//Called once data comes back from http endpoint 
+//Called once data comes back from http endpoint (HTTP request)
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=813%20nw%20oak%20ave%2097330&key=${api_key}`,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });

6e5d09987130d786600b04cb4cc70275b6f4e18c
Alaaddin Hiefield <alaaddinhiefield@Alaaddins-MacBook-Pro-2.local>
2019-01-05 13:01:29 -0800

    wrote a few lines for error handling

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const request = require('request');
 const fs = require('fs');
 const keys = require('dotenv').config();  // Get environment variables (API Key)
-const api_key = process.env.API_KEY;      // store API key for google maps
+const api_key = process.env.API_KEY;      // fetch stored API key for google maps
+const yargs = require('yargs');           // load in yargs
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: "address",
+            describe: "Address to fetch weather for.",
+            string: true                                        // always parse input as a string
+        }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+console.log(argv.a);
+let address = encodeURIComponent(argv.a);
+
 
 //Called once data comes back from http endpoint (HTTP request)
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=813%20nw%20oak%20ave%2097330&key=${api_key}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${api_key}`,
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    if (body.status === 'ZERO_RESULTS') {
+        // run this block if an error exists
+        console.log('Unable to find the given address.');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+    } else {
+        console.log("Unable to connect to Google Servers.");
+    }
+    
 });
